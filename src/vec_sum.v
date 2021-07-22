41d7fb5aa49eaa5a385f043ae2a9b3563df28d2c
Chanjung Kim <freiyer.paxbun@gmail.com>
2021-07-22 21:01:54 +0900

    Remove the pipelining register at front

--- src/vec_sum.v
+++ src/vec_sum.v
@@ -33,20 +33,22 @@ module vec_sum #(`VEC_PARAMS) (
             assign out = in;
         end else begin
             localparam depth = num_reducers(VEC_SIZE);
-            reg [(`VEC_WIDTH(VEC_SIZE) - 1) : 0] intm_in [(depth - 1) : 0];
+            reg [(`VEC_WIDTH(VEC_SIZE) - 1) : 0] intm_in [(depth - 1) : 1];
             wire [(`VEC_WIDTH(VEC_SIZE) - 1) : 0] intm_out [(depth - 1) : 0];
-            always @(posedge clk) begin
-                intm_in[0] <= in;
-            end
             assign out = intm_out[depth - 1];
             for (i = 0; i < depth; i = i + 1) begin
                 localparam in_size = input_width_at(VEC_SIZE, i);
                 localparam out_size = (in_size + 1) / 2;
-                vec_sum_reduce #(`FLOAT_PRPG_BIAS_PARAMS, .VEC_SIZE(in_size)) reducer (
-                    intm_in[i][(`VEC_WIDTH(in_size) - 1) : 0],
-                    intm_out[i][(`VEC_WIDTH(out_size) - 1) : 0]
-                );
-                if (i > 0) begin
+                if (i == 0) begin
+                    vec_sum_reduce #(`FLOAT_PRPG_BIAS_PARAMS, .VEC_SIZE(in_size)) reducer (
+                        in,
+                        intm_out[i][(`VEC_WIDTH(out_size) - 1) : 0]
+                    );
+                end else begin
+                    vec_sum_reduce #(`FLOAT_PRPG_BIAS_PARAMS, .VEC_SIZE(in_size)) reducer (
+                        intm_in[i][(`VEC_WIDTH(in_size) - 1) : 0],
+                        intm_out[i][(`VEC_WIDTH(out_size) - 1) : 0]
+                    );
                     always @(posedge clk) begin
                         intm_in[i][(`VEC_WIDTH(in_size) - 1) : 0]
                             <= intm_out[i - 1][(`VEC_WIDTH(in_size) - 1) : 0];
